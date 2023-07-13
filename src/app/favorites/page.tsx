f5eeebc6fc2ed9fe726a1e22e0ff1184e7b111e0
neo-athletics <juan109rodriguez@gmail.com>
2023-07-13 11:43:08 -0500

    finish implementation of drag and drop feature

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -6,16 +6,19 @@ import styles from "../page.module.css";
 import { useStore } from "../store";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { motion } from "framer-motion";
+import { motion, Reorder } from "framer-motion";
 
 const Favorites = () => {
-    const constraintsRef = useRef(null);
     const {
         favorites,
         addToAttended,
         removeFromAttended,
         attendedRestaurants,
     } = useStore((state) => state);
+    const [favs, setFavs] = useState(favorites);
+    useEffect(() => {
+        setFavs(favorites);
+    }, [favorites]);
 
     console.log(attendedRestaurants);
     const handleTruth = (restaurant) => {
@@ -27,60 +30,61 @@ const Favorites = () => {
         console.log("false");
         removeFromAttended(restaurant);
     };
+    //add and removing favorite is not working with implemetation of onReorder prop
     return (
-        <div className={styles.favoriteList} ref={constraintsRef}>
+        <div className={styles.favoriteList}>
             Favorites
-            {favorites.length > 0 && (
-                <>
-                    {favorites?.map((restaurant) => (
-                        <motion.div
-                            drag="y"
-                            whileDrag={{ scale: 1.1 }}
-                            key={restaurant.id}
-                            className={styles.favoriteItem}
-                            dragConstraints={constraintsRef}
-                        >
-                            <Image
-                                src={
-                                    restaurant?.image_url !== ""
-                                        ? restaurant?.image_url
-                                        : "/../public/coffee_img.jpg"
-                                }
-                                alt="restaurant food"
-                                width={200}
-                                height={150}
-                            />
-                            <Favorite restaurant={restaurant} />
-                            <FontAwesomeIcon
-                                icon={faCheck}
-                                size="xl"
-                                className={
-                                    attendedRestaurants.find(
-                                        (val) => restaurant.id === val.id
-                                    ) != undefined
-                                        ? styles.attended
-                                        : styles.notAttended
-                                }
-                                onClick={() =>
-                                    attendedRestaurants.find(
-                                        (val) => restaurant.id === val.id
-                                    ) === undefined
-                                        ? handleTruth(restaurant)
-                                        : handleFalse(restaurant)
-                                }
-                            />
-                            <p>{restaurant.name}</p>
-                            <p>rating: {restaurant.rating}</p>
-                            <p>reviews {restaurant.review_count}</p>
-                            <p>
-                                {restaurant.display_address[0] +
-                                    " " +
-                                    restaurant.display_address[1]}
-                            </p>
-                        </motion.div>
-                    ))}
-                </>
-            )}
+            <Reorder.Group axis="y" onReorder={setFavs} values={favs}>
+                {favs.length > 0 && (
+                    <>
+                        {favs?.map((restaurant) => (
+                            <Reorder.Item
+                                value={restaurant}
+                                key={restaurant.id}
+                                className={styles.favoriteItem}
+                            >
+                                <Image
+                                    src={
+                                        restaurant?.image_url !== ""
+                                            ? restaurant?.image_url
+                                            : "/../public/coffee_img.jpg"
+                                    }
+                                    alt="restaurant food"
+                                    width={200}
+                                    height={150}
+                                />
+                                <Favorite restaurant={restaurant} />
+                                <FontAwesomeIcon
+                                    icon={faCheck}
+                                    size="xl"
+                                    className={
+                                        attendedRestaurants.find(
+                                            (val) => restaurant.id === val.id
+                                        ) != undefined
+                                            ? styles.attended
+                                            : styles.notAttended
+                                    }
+                                    onClick={() =>
+                                        attendedRestaurants.find(
+                                            (val) => restaurant.id === val.id
+                                        ) === undefined
+                                            ? handleTruth(restaurant)
+                                            : handleFalse(restaurant)
+                                    }
+                                />
+                                <p>{restaurant.name}</p>
+                                <p>rating: {restaurant.rating}</p>
+                                <p>reviews {restaurant.review_count}</p>
+                                <p>
+                                    {restaurant.display_address[0] +
+                                        " " +
+                                        restaurant.display_address[1]}
+                                </p>
+                            </Reorder.Item>
+                        ))}
+                    </>
+                )}
+            </Reorder.Group>
         </div>
     );
 };
